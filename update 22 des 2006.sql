31a9566dff781fa563caa118ed5d251515e104ac
Genta Kode Giselle <C:\Users\Kode\AppData\Roaming\The Bat!>
2021-07-05 19:57:12 +0800

    hapus file yg tidak perlu

--- update 22 des 2006.sql
+++
@@ -1,4 +0,0 @@
-/*Update biaya administrasi tabungan beserta pos jurnalnya */
-ALTER TABLE tabungan ADD biayaadministrasi DOUBLE AFTER nopdl;
-ALTER TABLE tabungan ADD kasbank CHAR(20)  AFTER biayaadministrasi;
-ALTER TABLE tabungan ADD rekeningpendapatan CHAR(25)  AFTER kasbank;
